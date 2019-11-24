24269ebae8f5fe8bde582af1d2b6f438f35c6d4f
allenkis1990 <allenkis1990@163.com>
2019-11-24 22:09:22 +0800

    重新构建121212

--- mod-loader.js
+++
@@ -1,9 +0,0 @@
-
-import {lwh_demo2,lwh_demo2Components,lwh_demo2Routers,lwh_demo2Stores} from '@mods/@lwh-demo2/index'
-//模块多了后一个个合并进来
-export const Routers = [...lwh_demo2Routers]
-export const Stores = {...lwh_demo2Stores}
-export const Components = {...lwh_demo2Components}
-export default function(Vue){
-    Vue.use(lwh_demo2)
-}
